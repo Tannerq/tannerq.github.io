3168d054878dd1983cd09198e218001dcc9deeb5
tannerq <tannerq@umich.edu>
2020-07-28 15:59:14 -0700

    Updates

---
+++ precache-manifest.2ff4887f84b5837f9adfea99d8d4f265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef29ad8078402ce3a3bcc52bbe33958",
+    "url": "/showcase/index.html"
+  },
+  {
+    "revision": "e8d29c91f0f938517f64",
+    "url": "/showcase/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "520fdd9a933ab47bdd56",
+    "url": "/showcase/static/js/2.f1f4b618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showcase/static/js/2.f1f4b618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d29c91f0f938517f64",
+    "url": "/showcase/static/js/main.43f26358.chunk.js"
+  },
+  {
+    "revision": "520d3acac7b62759aab7",
+    "url": "/showcase/static/js/runtime-main.108a18e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/showcase/static/media/logo.5d5d9eef.svg"
+  }
+]);
